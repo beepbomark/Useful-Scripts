ea80da1f0851f763ba2ae67140c3866c1b02cdcb
beepopjake <68144485+beepbomark@users.noreply.github.com>
2023-05-01 20:45:14 +0800

    update

---
+++ Scripts/kill_adobe.bat
@@ -0,0 +1,4 @@
+@echo off
+taskkill /F /IM AcroRd64.exe /T
+taskkill /F /IM Acrobat.exe /T
+echo Adobe Reader and Acrobat Pro terminated.
---
+++ Scripts/kill_office.bat
@@ -0,0 +1,13 @@
+@echo off
+taskkill /F /IM excel.exe /T
+taskkill /F /IM winword.exe /T
+taskkill /F /IM powerpnt.exe /T
+taskkill /F /IM outlook.exe /T
+taskkill /F /IM lync.exe /T
+taskkill /F /IM onenote.exe /T
+taskkill /F /IM msaccess.exe /T
+taskkill /F /IM mspub.exe /T
+taskkill /F /IM visio.exe /T
+taskkill /F /IM infopath.exe /T
+taskkill /F /IM teams.exe /T
+echo All Office applications terminated.
